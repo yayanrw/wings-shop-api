8d69bf7bb7d779b949c5c0b011d76d05f52db221
yayanrw <yayanraw@gmail.com>
2023-06-24 11:03:15 +0700

    feat: create 2023_06_24_035520_create_transaction_details_table.php

---
+++ database/migrations/2023_06_24_035520_create_transaction_details_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaction_details', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('transaction_id');
+            $table->string('document_code', 3);
+            $table->string('document_number', 10);
+            $table->string('product_code', 18);
+            $table->unsignedBigInteger('price', 6)->default(0);
+            $table->unsignedBigInteger('quantity', 6)->default(0);
+            $table->string('unit', 5);
+            $table->unsignedBigInteger('sub_total', 10)->default(0);
+            $table->string('currency', 5);
+            $table->timestamps();
+        });
+
+        Schema::table('transaction_details', function ($table) {
+            $table->foreign('transaction_id')->references('id')->on('transactions')->onDelete('cascade');
+            $table->foreign('product_code')->references('code')->on('products')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaction_details');
+    }
+};
