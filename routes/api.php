34a4350571e20a2d23be90bc2797587a0dc75d2b
yayanrw <yayanraw@gmail.com>
2023-06-24 09:33:26 +0700

    add public routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Public routes
+Route::controller(AuthController::class)->group(function () {
+    Route::post('/login', 'login')->name('login');
+    Route::post('/register', 'register')->name('register');
+});
