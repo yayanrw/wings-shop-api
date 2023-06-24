cf01e8ea6051f3c364ff6753ccd195a2216a1194
yayanrw <yayanraw@gmail.com>
2023-06-24 10:04:51 +0700

    feat: create 2023_06_24_024917_create_products_table.php

---
+++ database/migrations/2023_06_24_024917_create_products_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('code', 18);
+            $table->string('name', 30);
+            $table->unsignedBigInteger('price')->nullable();
+            $table->string('currency', 5)->nullable();
+            $table->unsignedBigInteger('discount')->nullable();
+            $table->string('dimension', 50)->nullable();
+            $table->string('unit', 5)->nullable();
+            $table->timestamps();
+            $table->softDeletes('deleted_at', $precision = 0);
+            $table->unsignedBigInteger('created_by');
+            $table->unsignedBigInteger('updated_by')->nullable();
+            $table->unsignedBigInteger('deleted_by')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
