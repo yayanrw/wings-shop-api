88c9fdbcd000f43442d6594c42c241bcf39a44fd
yayanrw <yayanraw@gmail.com>
2023-06-24 11:12:48 +0700

    update TransactionDetail.php

--- app/Models/TransactionDetail.php
+++ app/Models/TransactionDetail.php
@@ -4,8 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class TransactionDetail extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'document_code',
+        'document_number',
+        'product_code',
+        'price',
+        'quantity',
+        'unit',
+        'sub_total',
+        'currency',
+    ];
+
+    public function transaction(): BelongsTo
+    {
+        return $this->belongsTo(Transaction::class);
+    }
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
