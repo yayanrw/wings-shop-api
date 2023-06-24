a0de58da909bb8361bab40df7b735d07a9be0731
yayanrw <yayanraw@gmail.com>
2023-06-24 11:25:52 +0700

    feat: create Cart.php

---
+++ app/Models/Cart.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Cart extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'product_code',
+        'price',
+        'quantity',
+        'unit',
+        'sub_total',
+        'currency',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
