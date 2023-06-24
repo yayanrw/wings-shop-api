e0bcfc879d8ae9ac7febdd201db8f1e3cbe281ac
yayanrw <yayanraw@gmail.com>
2023-06-24 10:52:22 +0700

    feat: create 2023_06_24_034519_create_transactions_table.php

---
+++ database/migrations/2023_06_24_034519_create_transactions_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->string('document_code');
+            $table->string('document_number');
+            $table->unsignedBigInteger('users_id');
+            $table->unsignedBigInteger('total')->default(0);
+            $table->date('date');
+            $table->timestamps();
+        });
+
+        Schema::table('transactions', function ($table) {
+            $table->foreign('users_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
